e8ba90bf0ea2a20a8775c3aec89614bdd61f2cab
Yash Modi <modiyash2007@gmail.com>
2025-07-07 17:00:23 -0400

    Changed files, created main.py which should be run instead of oscar_scraper.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from oscar_scraper import *
+
+debug_mode = True
+# CRNS variable imported from sensitive_info.py
+sleep_time_between_course_initialization = 1.0 # Must be float value, "*.***"
+courses = make_courses(CRNS,
+                       sleep_time_between_course_initialization,
+                       debug_mode)
+# Sleep times in seconds (same as above, must be floats)
+sleep_time_between_courses = 1.0
+sleep_time_between_each_ping = 5.0
+sleep_time_between_error = 30.0
+loop_check_courses(courses,
+                   sleep_time_between_courses,
+                   sleep_time_between_each_ping,
+                   sleep_time_between_error,
+                   debug_mode)
--- oscar_scraper.py
+++ oscar_scraper.py
@@ -74,20 +74,3 @@ def loop_check_courses(courses,
             # Other errors
             log("Fatal error occurred:", e)
             sys.exit(1)
-        
-
-debug_mode = True
-# CRNS variable imported from sensitive_info.py
-sleep_time_between_course_initialization = 1.0 # Must be float value, "*.***"
-courses = make_courses(CRNS,
-                       sleep_time_between_course_initialization,
-                       debug_mode)
-# Sleep times in seconds (same as above, must be floats)
-sleep_time_between_courses = 1.0
-sleep_time_between_each_ping = 5.0
-sleep_time_between_error = 30.0
-loop_check_courses(courses,
-                   sleep_time_between_courses,
-                   sleep_time_between_each_ping,
-                   sleep_time_between_error,
-                   debug_mode)
