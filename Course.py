5eba275283623f098a28022accb082dad20d738f
Yash Modi <modiyash2007@gmail.com>
2025-07-06 21:42:52 -0400

    update_nums_registered() works now

--- Course.py
+++ Course.py
@@ -1,7 +1,7 @@
 from sensitive_info import *
 
 import requests
-from bs4 import BeautifulSoup # type: ignore
+from bs4 import BeautifulSoup, Tag # type: ignore
 from typing import Optional
 
 class Course:
@@ -17,7 +17,7 @@ class Course:
         self.course_number = ""
         self.course_section = ""
         self.url = f"https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_detail_sched?term_in=202508&crn_in={self.course_crn}"
-        self.num_registered = 0
+        self.nums_registered = self.update_nums_registered()
         # Fetch course data when the object is created
         self.fetch_course_data()
 
@@ -28,9 +28,8 @@ class Course:
             # Check if the request was successful
             # If the request was successful, parse the HTML content
             if response.status_code == 200:
-                # self.update_num_registered()
                 html = response.content
-                self.update_num_registered(html)
+                self.update_nums_registered(html)
                 soup = BeautifulSoup(html, 'html.parser')
                 course_information = soup.find(class_='ddlabel', scope="row")
                 if course_information:
@@ -58,33 +57,41 @@ class Course:
         except requests.exceptions.RequestException as e:
             print("An error occurred while making the request:", e)
 
-    def update_num_registered(self, html=None):
+    def update_nums_registered(self, html=None) -> int:
         try:
+            # Finds html code if it is not inputted
             if html is None:
                 response = self.session.get(self.url)
                 if response.status_code == 200:
                     html = response.content
+                # Error handling for response
                 elif response.status_code == 404:
                     print("Page not found (404). Please check the URL.")
-                    return
+                    return -2
                 else:
                     print(f"Failed to retrieve the page. Status code: {response.status_code}")
-                    return
+                    return -2
             soup = BeautifulSoup(html, 'html.parser')
             # Example: Find the number of registered students (update selector as needed)
             table = soup.find('table', class_='datadisplaytable', summary='This layout table is used to present the seating numbers.')
-            print(table)
+            if isinstance(table, Tag):
+                self.nums_registered = list(map(lambda item: item.get_text(), (table.find_all('td', class_='dddefault'))))
+                return self.nums_registered[1]
+            else:
+                print("Could not find the seating numbers table.")
+            return -1
         except requests.exceptions.ConnectionError as e:
             print("Connection error. Please check your internet connection,", e)
+            return -1
         except requests.exceptions.Timeout as e:
             print("Connection timed out. Please check your internet connection,", e)
+            return -1
         except requests.exceptions.RequestException as e:
             print("An error occurred while making the request:", e)
+            return -1
         except Exception as e:
             print("An error occurred while parsing the HTML:", e)
-        
-        
-
+            return -1
 
     def __repr__(self):
         return f"Course({self.course_crn}, {self.course_title}, {self.course_department}, {self.course_number}, {self.course_section})"
