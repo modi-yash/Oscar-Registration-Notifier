5dc397888d8bbc7fe699792b7ef0b28cc6c48f1c
Yash Modi <modiyash2007@gmail.com>
2025-07-07 09:36:16 -0400

    put the creation of courses into a method called make_courses

--- oscar_scraper.py
+++ oscar_scraper.py
@@ -5,30 +5,32 @@ from Course import *
 from sensitive_info import *
 import sys
 
-courses = []
-with requests.Session() as session:
-    session.headers.update({'user-agent': USER_AGENT})
-    # Creates couses
-    for crn in CRNS:
-        num_tries = 0
-        while num_tries < 3:
-            try:
-                course = Course(crn, session)
-                courses.append(course)
-                break  # Success, exit retry loop
-            except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
-                num_tries += 1
-                log(f"Attempt {num_tries} for CRN {crn} failed: {e}")
-                if num_tries == 3:
-                    log(f"Giving up on CRN {crn} after 3 failed attempts.")
-                else:
-                    time.sleep(5)  # Wait before retrying
-            except Exception as e:
-                log(f"Error fetching data for CRN {crn}: {e}")
-                break
-        time.sleep(1)  # Sleep for 1 second to avoid overwhelming the server
-    print("Finished fetching data for all CRNs. Starting looping.")
+def make_courses():
+    all_courses_list = []
+    with requests.Session() as session:
+        session.headers.update({'user-agent': USER_AGENT})
+        # Creates couses
+        for crn in CRNS:
+            num_tries = 0
+            while num_tries < 3:
+                try:
+                    all_courses_list.append(Course(crn, session))
+                    break  # Success, exit retry loop
+                except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
+                    num_tries += 1
+                    log(f"Attempt {num_tries} for CRN {crn} failed: {e}")
+                    if num_tries == 3:
+                        log(f"Giving up on CRN {crn} after 3 failed attempts.")
+                    else:
+                        time.sleep(5)  # Wait before retrying
+                except Exception as e:
+                    log(f"Error fetching data for CRN {crn}: {e}")
+                    break
+            time.sleep(1)  # Sleep for 1 second to avoid overwhelming the server
+        log("Finished fetching data for all CRNs. Starting looping.")
+        return all_courses_list
 
+courses = make_courses()
 while(True):
     try:
         for course in courses:
